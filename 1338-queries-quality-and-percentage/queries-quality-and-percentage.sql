828fc79211d0c1105db1d6e0d81007f028d36672
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2025-11-01 14:26:13 +0530

    Time: 357 ms (42.66%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,6 @@
+SELECT 
+    query_name,
+     round(avg(rating/position), 2) as quality,
+       round(avg(if (rating<3, 1, 0))*100, 2) as poor_query_percentage 
+FROM Queries
+GROUP BY query_name;
