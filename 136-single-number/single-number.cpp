68d7ff2e3dfe84f7efa94daa3d17bfd34f53d539
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-06 22:57:38 +0530

    Time: 10 ms (14.14%) | Memory: 24 MB (7.66%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int> map;
+        for(int i=0;i<n;i++){
+            map[nums[i]]++;
+        }
+        for(auto it:map){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
