c37f6272cdb372ac7bb6779f990cff1cf6e257d0
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-10 18:23:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (67.63%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -9,7 +9,7 @@ public:
         //     }
         // }
         // matrix = ans;
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
