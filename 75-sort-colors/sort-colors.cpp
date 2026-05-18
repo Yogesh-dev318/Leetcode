19ec5c7b61f3023251fbc04786f6c4bceff1de73
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-18 19:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (46.99%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,37 +2,49 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
-
+        // int count0 = 0;
+        // int count1 = 0;
+        // int count2 = 0;
+        // vector<int> ans;
+        // for(int i = 0; i < n; i++) {
+        //     if(nums[i] == 0) {
+        //         count0++;
+        //     }
+        //     else if(nums[i] == 1) {
+        //         count1++;
+        //     }
+        //     else {
+        //         count2++;
+        //     }
+        // }
+        // for(int i = 0; i < count0; i++) {
+        //     ans.push_back(0);
+        // }
+        // for(int i = 0; i < count1; i++) {
+        //     ans.push_back(1);
+        // }
+        // for(int i = 0; i < count2; i++) {
+        //     ans.push_back(2);
+        // }
+        // nums=ans;
+        map<int,int> mp;
         vector<int> ans;
-
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == 0) {
-                count0++;
-            }
-            else if(nums[i] == 1) {
-                count1++;
-            }
-            else {
-                count2++;
-            }
+        for(int i=0;i<n;i++){
+            mp[nums[i]]+=1;
         }
-
+        int count0 = mp[0];
+        int count1 = mp[1];
+        int count2 = mp[2];
         for(int i = 0; i < count0; i++) {
             ans.push_back(0);
         }
-
         for(int i = 0; i < count1; i++) {
             ans.push_back(1);
         }
-
         for(int i = 0; i < count2; i++) {
             ans.push_back(2);
         }
-
         nums=ans;
+
     }
 };
