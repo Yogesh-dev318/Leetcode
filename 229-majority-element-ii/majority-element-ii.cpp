e132949a3e92dd1ee859f3e16506375a6f28a5fe
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-19 22:39:50 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (62.66%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,15 +2,57 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        map<int,int> mp;
+        // map<int,int> mp;
+        // vector<int> ans;
+        // for(int i=0;i<n;i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(auto it:mp){
+        //     if(it.second>n/3){
+        //         ans.push_back(it.first);
+        //     }
+        // }
+        // return ans;
         vector<int> ans;
+        int cnt1=0;
+        int cnt2=0;
+        int el1;
+        int el2;
         for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        for(auto it:mp){
-            if(it.second>n/3){
-                ans.push_back(it.first);
+            if(cnt1==0 && nums[i]!=el2){
+                cnt1=1;
+                el1=nums[i];
             }
+            else if(cnt2==0 && nums[i]!=el1){
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(el1==nums[i]){
+                cnt1++;
+            }
+            else if(el2==nums[i]){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==el1){
+                cnt1++;
+            }
+            else if(nums[i]==el2){
+                cnt2++;
+            }
+        }
+        if(cnt1>n/3){
+            ans.push_back(el1);
+        }
+        if(cnt2>n/3){
+            ans.push_back(el2);
         }
         return ans;
     }
