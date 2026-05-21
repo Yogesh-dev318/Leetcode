bf1aa5aa0e6203002af45d451148726f1c4208f3
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-21 22:50:01 +0530

    Time: 7 ms (5.78%) | Memory: 10.3 MB (15.07%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool pal(string &ans,int i,int j){
+        if(i>=j){
+            return true;
+        }
+        else if(ans[i]==ans[j]){
+            return pal(ans,i+1,j-1);
+        }
+        else if(ans[i]!=ans[j]){
+            return false;
+        }
+        return -1;
+    }
+    bool isPalindrome(string s) {
+        string ans;
+        for(int i=0;i<s.size();i++){
+            if(isalnum(s[i])){
+                ans+=tolower(s[i]);
+            }
+        }
+        return pal(ans,0,ans.size()-1);
+    }
+};
