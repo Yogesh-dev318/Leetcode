19895739156455b3f558b7e291e24f756bbc4969
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-21 17:07:26 +0530

    Time: 83 ms (51.91%) | Memory: 89.1 MB (16.43%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -7,45 +7,45 @@ public:
             return 0;
         }
 
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
 
-        int longest = 1;
-        int count = 1;
+        // int longest = 1;
+        // int count = 1;
 
-        for(int i = 1; i < n; i++) {
+        // for(int i = 1; i < n; i++) {
 
-            if(nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            else if(nums[i] == nums[i - 1] + 1) {
-                count++;
-            }
-
-            else {
-                count = 1;
-            }
-
-            longest = max(longest, count);
-        }
-
-        return longest;
-        // int longest=1;
-        // unordered_set<int> st;
-        // for(int i=0;i<n;i++){
-        //     st.insert(nums[i]);
-        // }
-        // for(auto it : st){
-        //     if(st.find(it - 1)==st.end()){
-        //         int count=1;
-        //         int x=it;
-        //         while(st.find(x+1)!=st.end()){
-        //             x=x+1;
-        //             count++;
-        //         }
-        //         longest=max(longest,count);
+        //     if(nums[i] == nums[i - 1]) {
+        //         continue;
         //     }
+
+        //     else if(nums[i] == nums[i - 1] + 1) {
+        //         count++;
+        //     }
+
+        //     else {
+        //         count = 1;
+        //     }
+
+        //     longest = max(longest, count);
         // }
+
         // return longest;
+        int longest=1;
+        unordered_set<int> st;
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+        }
+        for(auto it : st){
+            if(st.find(it - 1)==st.end()){
+                int count=1;
+                int x=it;
+                while(st.find(x+1)!=st.end()){
+                    x=x+1;
+                    count++;
+                }
+                longest=max(longest,count);
+            }
+        }
+        return longest;
     }
 };
