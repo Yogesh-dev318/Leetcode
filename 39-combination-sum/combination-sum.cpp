c77218baedaf1b9dc99ecf5964d1b570edf63aba
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-23 10:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (72.82%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void combine(int i,vector<int> &arr,int target,vector<vector<int>> &ans,vector<int> &d){
+        if(i==arr.size()){
+            if(target==0){
+                ans.push_back(d);
+            }
+            return;
+        }
+        if(arr[i]<=target){
+            d.push_back(arr[i]);
+            combine(i,arr,target-arr[i],ans,d);
+            d.pop_back();
+            // target=target+arr[i];
+        }
+        combine(i+1,arr,target,ans,d);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> d;
+        combine(0,candidates,target,ans,d);
+        return ans;
+    }
+};
