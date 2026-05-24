da76ff529faadc771d17a1d60338c6b955540a05
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-24 22:50:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (64.64%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    void subsetsum(int index,vector<int> &nums,vector<int> &ds,set<vector<int>> &st,int n){
-        if(index==n){
-            st.insert(ds);
-            return;
+    // void subsetsum(int index,vector<int> &nums,vector<int> &ds,set<vector<int>> &st,int n){
+    //     if(index==n){
+    //         st.insert(ds);
+    //         return;
+    //     }
+    //     ds.push_back(nums[index]);
+    //     subsetsum(index+1,nums,ds,st,n);
+    //     ds.pop_back();
+    //     subsetsum(index+1,nums,ds,st,n);
+
+    // }
+    void subsetsum(int index,vector<int> &nums,vector<int> &ds,vector<vector<int>> &ans){
+        // if(index==nums.size()){
+            
+        //     return;
+        // }
+        ans.push_back(ds);
+        for(int i=index;i<nums.size();i++){
+            if(i!=index && nums[i]==nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            subsetsum(i+1,nums,ds,ans);
+            ds.pop_back();
         }
-        ds.push_back(nums[index]);
-        subsetsum(index+1,nums,ds,st,n);
-        ds.pop_back();
-        subsetsum(index+1,nums,ds,st,n);
 
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // sort(nums.begin(),nums.end());
+        // vector<int> ds;
+        // set<vector<int>> st;
+        // subsetsum(0,nums,ds,st,n);
+        // vector<vector<int>> ans(st.begin(),st.end());
+        // // sort(ans.begin(),ans.end());
+        // return ans;
+        vector<vector<int>> ans;
         vector<int> ds;
-        set<vector<int>> st;
-        subsetsum(0,nums,ds,st,n);
-        vector<vector<int>> ans(st.begin(),st.end());
-        // sort(ans.begin(),ans.end());
+        sort(nums.begin(),nums.end());
+        subsetsum(0,nums,ds,ans);
         return ans;
     }
 };
