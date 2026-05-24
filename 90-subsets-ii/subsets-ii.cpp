8cf5caffa6aaddf3a9e292d89ad5de8c131aaef5
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-24 18:00:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (36.62%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void subsetsum(int index,vector<int> &nums,vector<int> &ds,set<vector<int>> &st,int n){
+        if(index==n){
+            st.insert(ds);
+            return;
+        }
+        ds.push_back(nums[index]);
+        subsetsum(index+1,nums,ds,st,n);
+        ds.pop_back();
+        subsetsum(index+1,nums,ds,st,n);
+
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int> ds;
+        set<vector<int>> st;
+        subsetsum(0,nums,ds,st,n);
+        vector<vector<int>> ans(st.begin(),st.end());
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
