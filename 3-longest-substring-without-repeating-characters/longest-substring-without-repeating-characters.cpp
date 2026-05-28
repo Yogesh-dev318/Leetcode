8d3dcf9a84ee6ea7ef4330611648e13705743aaf
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-28 16:11:48 +0530

    Time: 5 ms (68.83%) | Memory: 12 MB (46.69%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,35 +2,31 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n=s.size();
-        int count=0;
-        int ans=INT_MIN;
+        // int count=0;
+        // int ans=INT_MIN;
+        
         // for(int i=0;i<n;i++){
-        //     while(i<n){
-        //         if(st.find(s[i])==st.end()){
-        //             count++;
-        //             st.insert(s[i]);
-        //             ans=max(ans,count);
+        //     set<char> st;
+        //     for(int j=i;j<n;j++){
+        //         if(st.find(s[j])!=st.end()){
+        //             break;
         //         }
-        //         else{
-        //             st.clear();
-        //             count=1;
-        //             st.insert(s[i]);
-        //             ans=max(ans,count);
-        //         }
-        //         i++;
+        //         st.insert(s[j]);
+        //         ans=max(ans,j-i+1);
         //     }
         // }
         // return ans == INT_MIN ? 0 : ans;
-        for(int i=0;i<n;i++){
-            set<char> st;
-            for(int j=i;j<n;j++){
-                if(st.find(s[j])!=st.end()){
-                    break;
-                }
-                st.insert(s[j]);
-                ans=max(ans,j-i+1);
+        map<char, int> mp;
+        int l = 0;
+        int ans = 0;
+        for (int r = 0; r < n; r++) {
+            if (mp.find(s[r]) != mp.end() && mp[s[r]] >= l) {
+                l = mp[s[r]] + 1;
             }
+            mp[s[r]] = r;
+            ans = max(ans, r - l + 1);
         }
-        return ans == INT_MIN ? 0 : ans;
+        return ans;
     }
+    
 };
