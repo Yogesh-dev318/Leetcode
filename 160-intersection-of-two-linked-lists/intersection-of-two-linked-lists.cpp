f58ec536b0d5800d50bfe5e31b2e4cef8b42bb53
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-05-31 19:20:59 +0530

    Time: 53 ms (77.80%) | Memory: 24 MB (24.34%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,20 +9,31 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        map<ListNode*,int>mp;
-        ListNode* temp1=headA;
-        ListNode* temp2=headB;
-        while(temp1!=nullptr){
-            mp[temp1]++;
-            temp1=temp1->next;
+        // map<ListNode*,int>mp;
+        // ListNode* temp1=headA;
+        // ListNode* temp2=headB;
+        // while(temp1!=nullptr){
+        //     mp[temp1]++;
+        //     temp1=temp1->next;
+        // }
+        // while(temp2!=nullptr){
+        //     if(mp.find(temp2)!=mp.end()){
+        //         return temp2;
+        //     }
+        //     mp[temp2]++;
+        //     temp2=temp2->next;
+        // }
+        // return nullptr;
+        if(headA==nullptr || headB==nullptr) return nullptr;
+        ListNode* t1=headA;
+        ListNode* t2=headB;
+        while(t1!=t2){
+            t1=t1->next;
+            t2=t2->next;
+            if(t1==t2) return t1;
+            if(t1==nullptr) t1=headB;
+            if(t2==nullptr) t2=headA;
         }
-        while(temp2!=nullptr){
-            if(mp.find(temp2)!=mp.end()){
-                return temp2;
-            }
-            mp[temp2]++;
-            temp2=temp2->next;
-        }
-        return nullptr;
+        return t1;
     }
 };
