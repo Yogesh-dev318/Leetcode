4fc431d9f74b9dbdcd30d1d422892bf936d1dc05
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-06-01 19:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (31.05%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -11,24 +11,52 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        vector<int> arr;
+        // vector<int> arr;
+        // ListNode* temp=head;
+        // while(temp!=nullptr){
+        //     arr.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // int n=arr.size();
+        // vector<int> arr2(n);
+        // for(int i=0;i<n;i++){
+        //     arr2[(i+k)%n]=arr[i];
+        // }
+        // temp=head;
+        // int i=0;
+        // while(temp!=nullptr){
+        //     temp->val=arr2[i];
+        //     i++;
+        //     temp=temp->next;
+        // }
+        // return head;
+        if(head==nullptr){
+            return head;
+        }
+        int n=0;
         ListNode* temp=head;
         while(temp!=nullptr){
-            arr.push_back(temp->val);
+            n++;
             temp=temp->next;
         }
-        int n=arr.size();
-        vector<int> arr2(n);
-        for(int i=0;i<n;i++){
-            arr2[(i+k)%n]=arr[i];
+        k=k%n;
+        if(k%n==0){
+            return head;
         }
         temp=head;
-        int i=0;
-        while(temp!=nullptr){
-            temp->val=arr2[i];
-            i++;
+        while(temp->next!=nullptr){
             temp=temp->next;
         }
+        temp->next=head;
+        int i=n-k;
+        temp=head;
+        while(i!=1){
+            i--;
+            temp=temp->next;
+        }
+        head=temp->next;
+        temp->next=nullptr;
         return head;
+        
     }
 };
