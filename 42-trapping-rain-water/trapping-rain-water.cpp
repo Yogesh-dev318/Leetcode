9f3d9b60aaf17959b7e4d587998fa9ad4251f9eb
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-06-06 13:34:02 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (16.18%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    // int prefixmax(int i,vector<int>& height){
+    //     int n=height.size();
+    //     vector<int> prefix(n);
+    //     prefix[0]=height[0];
+    //     for(int i=1;i<n;i++){
+    //         prefix[i]=max(prefix[i-1],height[i]);
+    //     }
+    //     return prefix[i];
+    // }
+    // int suffixmax(int i,vector<int>& height){
+    //     int n=height.size();
+    //     vector<int> suffix(n);
+    //     suffix[n-1]=height[n-1];
+    //     for(int i=n-2;i>=0;i--){
+    //         suffix[i]=max(suffix[i+1],height[i]);
+    //     }
+    //     return suffix[i];
+    // }
+    int trap(vector<int>& height) {
+        int total=0;
+        int n=height.size();
+        vector<int> suffix(n);
+        suffix[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=max(suffix[i+1],height[i]);
+        }
+        vector<int> prefix(n);
+        prefix[0]=height[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=max(prefix[i-1],height[i]);
+        }
+        for(int i=0;i<n;i++){
+            int leftmax=prefix[i];
+            int rightmax=suffix[i];
+            if(height[i]<leftmax && height[i]<rightmax){
+                total+=min(leftmax,rightmax)-height[i];
+            }
+        }
+        return total;
+    }
+};
