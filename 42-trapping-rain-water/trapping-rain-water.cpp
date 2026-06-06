63b003a651f06f4ed7caec7319d346ff79b5710c
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-06-06 13:34:10 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (16.18%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-    // int prefixmax(int i,vector<int>& height){
-    //     int n=height.size();
-    //     vector<int> prefix(n);
-    //     prefix[0]=height[0];
-    //     for(int i=1;i<n;i++){
-    //         prefix[i]=max(prefix[i-1],height[i]);
-    //     }
-    //     return prefix[i];
-    // }
-    // int suffixmax(int i,vector<int>& height){
-    //     int n=height.size();
-    //     vector<int> suffix(n);
-    //     suffix[n-1]=height[n-1];
-    //     for(int i=n-2;i>=0;i--){
-    //         suffix[i]=max(suffix[i+1],height[i]);
-    //     }
-    //     return suffix[i];
-    // }
     int trap(vector<int>& height) {
         int total=0;
         int n=height.size();
