31dfed4d2fc6287186c046b81f8836af8cbd5f0b
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-06-07 22:36:22 +0530

    Time: 32 ms (52.84%) | Memory: 92 MB (58.04%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,18 +1,27 @@
 class StockSpanner {
 public:
     vector<int> list;
+    stack<pair<int,int>> st;
+    int index=-1;
     StockSpanner() {
     }
     int next(int price) {
-        list.push_back(price);
-        int cnt = 1;
-        for (int i = list.size() - 2; i >= 0; i--) {
-            if (list[i] <= price) {
-                cnt++;
-            } else {
-                break;
-            }
+        // list.push_back(price);
+        // int cnt = 0;
+        // for (int i = list.size() - 1; i >= 0; i--) {
+        //     if (list[i] <= price) {
+        //         cnt++;
+        //     } else {
+        //         break;
+        //     }
+        // }
+        // return cnt;
+        index++;
+        while (!st.empty() && st.top().first <= price) {
+            st.pop();
         }
-        return cnt;
+        int ans = index - (st.empty() ? -1 : st.top().second);
+        st.push({price, index});
+        return ans;
     }
 };
