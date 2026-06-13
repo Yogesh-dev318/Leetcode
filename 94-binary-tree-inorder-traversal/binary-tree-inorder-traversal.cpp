8745412ad952ab866d214dac3aa3d7ce27559684
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-06-13 17:15:45 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (12.61%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -20,8 +20,26 @@ public:
         inorder(root->right,ans);
     }
     vector<int> inorderTraversal(TreeNode* root) {
+        // vector<int> ans;
+        // inorder(root,ans);
+        // return ans;
         vector<int> ans;
-        inorder(root,ans);
+        stack<TreeNode*> st;
+        TreeNode* node=root;
+        while(true){
+            if(node!=NULL){
+                st.push(node);
+                node=node->left;
+            }
+            else{
+                if(st.empty()) break;
+                node=st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node=node->right;
+            }
+        }
         return ans;
+        
     }
 };
