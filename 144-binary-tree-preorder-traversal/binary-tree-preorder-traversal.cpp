1b974e3687ae28795f252ec842cf87cf8aae3204
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-06-13 16:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (72.77%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -20,8 +20,20 @@ public:
         preorder(root->right,ans);
     }
     vector<int> preorderTraversal(TreeNode* root) {
+        // vector<int> ans;
+        // preorder(root,ans);
+        // return ans;
         vector<int> ans;
-        preorder(root,ans);
+        stack<TreeNode*> st;
+        if(root==NULL) return ans;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* temp=st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            if(temp->right!=NULL) st.push(temp->right);
+            if(temp->left!=NULL) st.push(temp->left);
+        }
         return ans;
     }
 };
