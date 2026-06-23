5226c0d514898fe20062bbcaf141ae18d4dc7f61
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2026-06-23 12:41:37 +0530

    Time: 0 ms (100.00%) | Memory: 60.9 MB (51.32%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -10,30 +10,56 @@
  * };
  */
 class Solution {
+private: 
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* middle;
+    TreeNode* last; 
 public:
-    void inorder(TreeNode* root,vector<int>& arr){
-        if(root==NULL){
-            return;
+    // void inorder(TreeNode* root,vector<int>& arr){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     inorder(root->left,arr);
+    //     arr.push_back(root->val);
+    //     inorder(root->right,arr);
+    // }
+    // void setnodes(TreeNode* root,vector<int> arr,int& i){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     if(i==arr.size()){return;}
+    //     setnodes(root->left,arr,i);
+    //     root->val = arr[i];
+    //     i++;
+    //     setnodes(root->right,arr,i);
+    // }
+    void inorder(TreeNode* root) {
+        if(root == NULL) return; 
+        inorder(root->left);
+        if (prev != NULL && (root->val < prev->val))
+        {
+            if ( first == NULL )
+            {
+                first = prev;
+                middle = root;
+            }
+            else
+                last = root;
         }
-        inorder(root->left,arr);
-        arr.push_back(root->val);
-        inorder(root->right,arr);
-    }
-    void setnodes(TreeNode* root,vector<int> arr,int& i){
-        if(root==NULL){
-            return;
-        }
-        if(i==arr.size()){return;}
-        setnodes(root->left,arr,i);
-        root->val = arr[i];
-        i++;
-        setnodes(root->right,arr,i);
+        prev = root;
+        inorder(root->right); 
     }
     void recoverTree(TreeNode* root) {
-        vector<int> arr;
-        inorder(root,arr);
-        sort(arr.begin(),arr.end());
-        int i=0;
-        setnodes(root,arr,i);
+        // vector<int> arr;
+        // inorder(root,arr);
+        // sort(arr.begin(),arr.end());
+        // int i=0;
+        // setnodes(root,arr,i);
+        first = middle = last = NULL; 
+        prev = new TreeNode(INT_MIN); 
+        inorder(root);
+        if(first && last) swap(first->val, last->val); 
+        else if(first && middle) swap(first->val, middle->val);
     }
 };
